dec7847b4d2c95066f8ca0d8e1ce6d5d86ebda3b
Rupesh Tiwari <roopkt@gmail.com>
2022-01-15 18:58:42 -0500

    chore:updated

--- docs/docs/docs/docs/docs/docs/docs/docs/docs/build-release.bat
+++
@@ -1,7 +0,0 @@
-set JEKYLL_ENV=production | bundle exec jekyll build
-
-Xcopy "C:\Rupesh\Blog-SourceCode\rupeshtiwari.github.io\_site" "C:\Rupesh\Blog-SourceCode\rupeshtiwari.github.io\docs" /h /i /c /k /e /r /y
-
-git add .
-git commit -m 'blog updates'
-git push
--- docs/docs/docs/docs/docs/site_ghpages/copy to github pages branch.bat
+++
@@ -1,2 +0,0 @@
-bundle exec jekyll build
-Xcopy "C:\Rupesh\Blog-SourceCode\rupeshtiwari.github.io\_site" "C:\Rupesh\Blog-SourceCode\rupeshtiwari.github.io\site_ghpages" /h /i /c /k /e /r /y
