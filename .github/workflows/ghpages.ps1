01894964c9e58711473debe9e361625fe79d8ba5
Rupesh Tiwari <roopkt@gmail.com>
2022-01-31 11:04:17 -0500

    fixing workflow

--- .github/workflows/ghpages.ps1
+++ .github/workflows/ghpages.ps1
@@ -1,44 +1,18 @@
-Function Cleanup {
-    Set-Location go to docs folder 
-    $Folder = "./_site/docs"
-  
-    if (Test-Path -Path $Folder) {
-        # Deleting content of the docs folder
-        Get-ChildItem -Path $Folder -Include * -File -Recurse | ForEach-Object { $_.Delete() }
-        Write-Host Removed _site/docs folder
-    }
-}
-
-Function Build{
+Function Build {
     Write-Host Building Project
     Set-Variable JEKYLL_ENV=production 
-    bundle exec jekyll build -q
+    bundle exec jekyll build -q -d docs
     Write-Host Project build is complete
 }
-
-## Cleanup 
-Cleanup
 
 ## Build 
 Build
 
-## Create Docs folder
-Copy-Item -Path "./_site/*" -Destination "docs" -Recurse -Force
-Write-Host Build copied to docs folder 
-
-
 ## Push github
 Write-Host Pushing docs to gh_pages branch
+Set-Location C:\Rupesh\Blog-SourceCode\rupeshtiwari.github.io
 git checkout gh_pages
 git pull 
-
-# Clean
-$Folder = "./docs/docs"
-if (Test-Path -Path $Folder) {
-    Remove-Item $Folder -Force -Recurse
-}
-
-git add .
-git commit -m "blog updates"
+git commit -am "blog updates"
 git push
 
